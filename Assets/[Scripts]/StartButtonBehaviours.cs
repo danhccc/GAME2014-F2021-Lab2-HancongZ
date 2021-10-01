2448c46fe79d86d80caa9e1689c1835e04a79d80
Hancong Zhang <daniel94126@hotmail.com>
2021-10-01 19:42:33 -0400

    Initial project
    Copied from Lab 1

---
+++ Assets/[Scripts]/StartButtonBehaviours.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonBehaviours : MonoBehaviour
+{
+    public void OnStartButtonPressed()
+    {
+        Debug.Log("Start Button Pressed!");
+    }
+
+}
